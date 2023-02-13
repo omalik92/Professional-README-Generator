46e7f62862647729a0eb54144a7e87f8d7aa4e79
Omar <omalik0192@googlemail.com>
2023-02-13 11:24:37 +0000

    questions/prompts added to index.js for inquirer

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a breif description of your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide the installation intructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter the usage information for your project",
+  },
+  {
+    type: "list",
+    name: "licence",
+    message: "Choose a licence for you project:",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
 
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter the contributon guidlines for your project",
+  },
+  {
+    type: "input",
+    name: "gitubUsername",
+    message: "Enter your github username",
+  },
+
+  { type: "input", name: "email", message: "Enter you email address" },
 ];
 
+inquirer.prompt(questions).then((response) => console.log(response));
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
